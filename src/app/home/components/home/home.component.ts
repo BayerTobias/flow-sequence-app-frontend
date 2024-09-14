c2ecbbe092b4467bbc2aeaedf17503ace79b0ed8
BayerTobias <tbayer2@gmx.de>
2024-09-14 15:40:57 +0200

    media contols component

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from '../welcome/welcome.component';
 import { TimerComponent } from '../timer/timer.component';
+import { MediaControlsComponent } from '../media-controls/media-controls.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WelcomeComponent, TimerComponent],
+  imports: [WelcomeComponent, TimerComponent, MediaControlsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
---
+++ src/app/home/components/media-controls/media-controls.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-controls',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-controls.component.html',
+  styleUrl: './media-controls.component.scss'
+})
+export class MediaControlsComponent {
+
+}
