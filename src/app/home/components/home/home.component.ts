6fc7ef2b8d6972934cd73cafc4b987ac9fb60755
BayerTobias <tbayer2@gmx.de>
2024-09-14 14:22:50 +0200

    timer component

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from '../welcome/welcome.component';
+import { TimerComponent } from '../timer/timer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WelcomeComponent],
+  imports: [WelcomeComponent, TimerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  timer: boolean = true;
+}
