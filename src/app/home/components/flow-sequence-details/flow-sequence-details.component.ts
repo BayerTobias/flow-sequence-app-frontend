0beff184d00b66d78e45f8497e4886ec0c236bd9
BayerTobias <tbayer2@gmx.de>
2024-09-16 21:30:19 +0200

    flow sequence details html and scss

--- src/app/home/components/flow-sequence-details/flow-sequence-details.component.ts
+++ src/app/home/components/flow-sequence-details/flow-sequence-details.component.ts
@@ -3,16 +3,19 @@ import { FlowSequence } from '../../../models/flow-sequence.model';
 import { FlowTime } from '../../../models/flow-time.model';
 import { ShortBreak } from '../../../models/short-break.model';
 import { LongBreak } from '../../../models/long-break.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-flow-sequence-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './flow-sequence-details.component.html',
   styleUrl: './flow-sequence-details.component.scss',
 })
 export class FlowSequenceDetailsComponent {
-  flowSequence: FlowSequence = new FlowSequence();
+  public flowSequence: FlowSequence = new FlowSequence();
+
+  public completeColor: string = 'rgb(106, 158, 157)';
 
   ngOnInit() {
     // Only for Testing DELETE later
@@ -20,8 +23,8 @@ export class FlowSequenceDetailsComponent {
       new FlowTime({
         name: 'Programmieren',
         type: 'flowTime',
-        position: 3,
-        complete: false,
+        position: 1,
+        complete: true,
         duration: 60,
       })
     );
@@ -40,11 +43,340 @@ export class FlowSequenceDetailsComponent {
       new LongBreak({
         name: 'Lange Pause',
         type: 'longBreak',
-        position: 1,
+        position: 3,
         complete: false,
         duration: 30,
       })
     );
+
+    this.flowSequence.addStep(
+      new FlowTime({
+        name: 'Programmieren',
+        type: 'flowTime',
+        position: 1,
+        complete: true,
+        duration: 60,
+      })
+    );
+
+    this.flowSequence.addStep(
+      new ShortBreak({
+        name: 'Kurze Pause',
+        type: 'shortBreak',
+        position: 2,
+        complete: false,
+        duration: 5,
+      })
+    );
+
+    this.flowSequence.addStep(
+      new LongBreak({
+        name: 'Lange Pause',
+        type: 'longBreak',
+        position: 3,
+        complete: false,
+        duration: 30,
+      })
+    );
+
+    this.flowSequence.addStep(
+      new FlowTime({
+        name: 'Programmieren',
+        type: 'flowTime',
+        position: 1,
+        complete: true,
+        duration: 60,
+      })
+    );
+
+    this.flowSequence.addStep(
+      new ShortBreak({
+        name: 'Kurze Pause',
+        type: 'shortBreak',
+        position: 2,
+        complete: false,
+        duration: 5,
+      })
+    );
+
+    this.flowSequence.addStep(
+      new LongBreak({
+        name: 'Lange Pause',
+        type: 'longBreak',
+        position: 3,
+        complete: false,
+        duration: 30,
+      })
+    );
+
+    this.flowSequence.addStep(
+      new FlowTime({
+        name: 'Programmieren',
+        type: 'flowTime',
+        position: 1,
+        complete: true,
+        duration: 60,
+      })
+    );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new FlowTime({
+    //     name: 'Programmieren',
+    //     type: 'flowTime',
+    //     position: 1,
+    //     complete: true,
+    //     duration: 60,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new FlowTime({
+    //     name: 'Programmieren',
+    //     type: 'flowTime',
+    //     position: 1,
+    //     complete: true,
+    //     duration: 60,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new FlowTime({
+    //     name: 'Programmieren',
+    //     type: 'flowTime',
+    //     position: 1,
+    //     complete: true,
+    //     duration: 60,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new FlowTime({
+    //     name: 'Programmieren',
+    //     type: 'flowTime',
+    //     position: 1,
+    //     complete: true,
+    //     duration: 60,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new FlowTime({
+    //     name: 'Programmieren',
+    //     type: 'flowTime',
+    //     position: 1,
+    //     complete: true,
+    //     duration: 60,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
+    // this.flowSequence.addStep(
+    //   new FlowTime({
+    //     name: 'Programmieren',
+    //     type: 'flowTime',
+    //     position: 1,
+    //     complete: true,
+    //     duration: 60,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new FlowTime({
+    //     name: 'Programmieren',
+    //     type: 'flowTime',
+    //     position: 1,
+    //     complete: true,
+    //     duration: 60,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new FlowTime({
+    //     name: 'Programmieren',
+    //     type: 'flowTime',
+    //     position: 1,
+    //     complete: true,
+    //     duration: 60,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new ShortBreak({
+    //     name: 'Kurze Pause',
+    //     type: 'shortBreak',
+    //     position: 2,
+    //     complete: false,
+    //     duration: 5,
+    //   })
+    // );
+
+    // this.flowSequence.addStep(
+    //   new LongBreak({
+    //     name: 'Lange Pause',
+    //     type: 'longBreak',
+    //     position: 3,
+    //     complete: false,
+    //     duration: 30,
+    //   })
+    // );
 
     this.flowSequence.sortSteps();
     console.log(this.flowSequence);
