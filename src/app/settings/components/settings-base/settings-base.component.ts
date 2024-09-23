8a35625929711a10f062e4e695936bb45ce82bfb
BayerTobias <tbayer2@gmx.de>
2024-09-23 21:11:33 +0200

    settings general component

--- src/app/settings/components/settings-base/settings-base.component.ts
+++ src/app/settings/components/settings-base/settings-base.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { SettingsGeneralComponent } from '../settings-general/settings-general.component';
 
 @Component({
   selector: 'app-settings-base',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SettingsGeneralComponent],
   templateUrl: './settings-base.component.html',
   styleUrl: './settings-base.component.scss',
 })
