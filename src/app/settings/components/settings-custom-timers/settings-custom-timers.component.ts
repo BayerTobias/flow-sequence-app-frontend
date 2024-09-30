d1f4e70a47b84febf74c9373300a8bc0212582a1
BayerTobias <tbayer2@gmx.de>
2024-09-30 20:00:07 +0200

    mateial design drag and drop

--- src/app/settings/components/settings-custom-timers/settings-custom-timers.component.ts
+++ src/app/settings/components/settings-custom-timers/settings-custom-timers.component.ts
@@ -1,11 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { SimpleSettingsButtonComponent } from '../../../shared/components/buttons/simple-settings-button/simple-settings-button.component';
 import { SettingsServiceService } from '../../../shared/services/settings-service.service';
+import {
+  CdkDragDrop,
+  DragDropModule,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-settings-custom-timers',
   standalone: true,
-  imports: [SimpleSettingsButtonComponent],
+  imports: [SimpleSettingsButtonComponent, DragDropModule],
   templateUrl: './settings-custom-timers.component.html',
   styleUrl: './settings-custom-timers.component.scss',
 })
@@ -18,5 +23,13 @@ export class SettingsCustomTimersComponent {
 
   chooseSequence() {
     console.log('choose');
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(
+      this.settingsService.savedCustomSequences,
+      event.previousIndex,
+      event.currentIndex
+    );
   }
 }
