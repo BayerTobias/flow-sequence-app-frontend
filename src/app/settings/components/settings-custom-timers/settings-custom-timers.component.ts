722c927e52ea39a67be8b2e9388cb09889b5e165
BayerTobias <tbayer2@gmx.de>
2024-10-03 16:54:32 +0200

    create flow sequence steps

--- src/app/settings/components/settings-custom-timers/settings-custom-timers.component.ts
+++ src/app/settings/components/settings-custom-timers/settings-custom-timers.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { SimpleSettingsButtonComponent } from '../../../shared/components/buttons/simple-settings-button/simple-settings-button.component';
 import { SettingsServiceService } from '../../../shared/services/settings-service.service';
 import {
@@ -9,11 +15,20 @@ import {
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { FlowSequence } from '../../../models/flow-sequence.model';
+import { FlowTime } from '../../../models/flow-time.model';
+import { ShortBreak } from '../../../models/short-break.model';
+import { LongBreak } from '../../../models/long-break.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Step } from '../../../models/step.model';
 
 @Component({
   selector: 'app-settings-custom-timers',
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
     SimpleSettingsButtonComponent,
     DragDropModule,
     MatFormFieldModule,
@@ -26,9 +41,14 @@ import { MatInputModule } from '@angular/material/input';
 export class SettingsCustomTimersComponent {
   public settingsService = inject(SettingsServiceService);
 
-  public flowTimeDuration: number = 25;
+  public flowTimeDuration: number = 30;
   public shortBreakDuration: number = 5;
   public longBreakDuration: number = 15;
+
+  public newFlowSequence: FlowSequence = new FlowSequence();
+
+  @ViewChildren('nameInput') nameInputFields: QueryList<ElementRef> | null =
+    null;
 
   editSequence() {
     console.log('edit');
@@ -46,15 +66,92 @@ export class SettingsCustomTimersComponent {
     );
   }
 
+  dropCreateSequence(event: CdkDragDrop<string[]>) {
+    moveItemInArray(
+      this.newFlowSequence.steps,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+
   addFlowTime() {
-    console.log('Flow Time:', this.flowTimeDuration);
+    const step = new FlowTime({
+      name: '',
+      type: 'flowTime',
+      position: 0,
+      complete: false,
+      duration: this.flowTimeDuration,
+    });
+    this.newFlowSequence.addStep(step);
+    this.asignPostion();
+    console.log(this.newFlowSequence);
+
+    setTimeout(() => {
+      this.focusLastImput();
+    }, 1);
   }
 
   addShortBreak() {
-    console.log('Short Break:', this.shortBreakDuration);
+    const step = new ShortBreak({
+      name: 'Short Break',
+      type: 'shortBreak',
+      position: 0,
+      complete: false,
+      duration: this.shortBreakDuration,
+    });
+    this.newFlowSequence.addStep(step);
+    this.asignPostion();
   }
 
   addLongBreak() {
-    console.log('Long Break:', this.longBreakDuration);
+    const step = new LongBreak({
+      name: 'Long Break',
+      type: 'longBreak',
+      position: 0,
+      complete: false,
+      duration: this.longBreakDuration,
+    });
+    this.newFlowSequence.addStep(step);
+    this.asignPostion();
+  }
+
+  focusLastImput() {
+    if (this.nameInputFields && this.nameInputFields.length > 0) {
+      const lastInput = this.nameInputFields.last;
+
+      if (lastInput) lastInput.nativeElement.focus();
+    }
+  }
+
+  renameStep(step: Step, event: Event) {
+    const target = event.target as HTMLInputElement;
+
+    if (target) {
+      step.name = target.value;
+    }
+  }
+
+  editStepDuration(step: Step, event: Event) {
+    const target = event.target as HTMLInputElement;
+
+    if (target) {
+      step.duration = +target.value;
+    }
+  }
+
+  copyStep(step: Step) {
+    const copiedStep: Step = { ...step };
+
+    this.newFlowSequence.steps.push(copiedStep);
+  }
+
+  deleteStep(index: number) {
+    this.newFlowSequence.steps.splice(index, 1);
+  }
+
+  asignPostion() {
+    this.newFlowSequence.steps.forEach((step, index) => {
+      step.position = index;
+    });
   }
 }
