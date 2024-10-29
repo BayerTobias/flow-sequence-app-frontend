eba9a910d22a1b75dbe1a503e12a6689ebe5cc85
BayerTobias <tbayer2@gmx.de>
2024-10-29 18:56:02 +0100

    button component height and width inputs variables

--- src/app/shared/components/buttons/simple-settings-button/simple-settings-button.component.ts
+++ src/app/shared/components/buttons/simple-settings-button/simple-settings-button.component.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-simple-settings-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './simple-settings-button.component.html',
   styleUrl: './simple-settings-button.component.scss',
 })
 export class SimpleSettingsButtonComponent {
   @Input() content: string = '';
+  @Input() width: number | null = null;
+  @Input() height: number | null = null;
+
   @Output() submitEvent = new EventEmitter<void>();
 
   submit() {
