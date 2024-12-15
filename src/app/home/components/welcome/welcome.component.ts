c0b54eab058af532b151971ad0bf0965a9ea7a3c
BayerTobias <tbayer2@gmx.de>
2024-12-15 16:32:42 +0100

    display users first name as welcome msg

--- src/app/home/components/welcome/welcome.component.ts
+++ src/app/home/components/welcome/welcome.component.ts
@@ -1,18 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { GlassButtonComponent } from '../../../shared/components/buttons/glass-button/glass-button.component';
 import { FlowSequenceServiceService } from '../../../shared/services/flow-sequence-service.service';
 import { SettingsServiceService } from '../../../shared/services/settings-service.service';
+import { AuthService } from '../../../auth/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [GlassButtonComponent],
+  imports: [GlassButtonComponent, CommonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss',
 })
 export class WelcomeComponent {
   private flowSequenceService = inject(FlowSequenceServiceService);
   private settingsService = inject(SettingsServiceService);
+  public authService = inject(AuthService);
+  public displayName: string | null = null;
+
+  constructor() {
+    effect(() => {
+      const fullName = this.authService.userSignal()?.displayName;
+
+      if (fullName) {
+        const firstName = fullName.split(' ')[0];
+        this.displayName = ' ' + firstName;
+      } else {
+        this.displayName = '';
+      }
+    });
+  }
 
   startStandardSequence() {
     const standardSequence = this.settingsService.standardSequence;
