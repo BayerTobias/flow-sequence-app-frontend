7bdd13e12aae28900e789e37d76b8ab3f8d6efcc
BayerTobias <tbayer2@gmx.de>
2025-02-18 12:07:15 +0100

    privacy policy and tos components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
 import { WelcomeComponent } from './home/components/welcome/welcome.component';
 import { FlowSequenceTimerComponent } from './home/components/flow-sequence-timer/flow-sequence-timer.component';
+import { PrivacyPolicyComponent } from './shared/components/footer/privacy-policy/privacy-policy.component';
+import { TermsConditionsComponent } from './shared/components/footer/terms-conditions/terms-conditions.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'flowsequencetimer', component: FlowSequenceTimerComponent },
-  { path: 'privacy-policy', component: WelcomeComponent },
-  { path: 'imprint', component: WelcomeComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'tos', component: TermsConditionsComponent },
 ];
